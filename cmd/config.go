47fa84403ea36ba0cca397410d80883a4ef12c24
mmuuripe <markus.muuripeal@gmail.com>
2024-06-14 11:38:38 +0300

    server + config files

---
+++ cmd/config.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"gopkg.in/ini.v1"
+	"log"
+	"strings"
+	"github.com/gin-contrib/cors"
+)
+
+func loadCorsConfig() cors.Config {
+	cfg, err := ini.Load("config.cfg")
+	if err != nil {
+		log.Fatalf("Failed to read config file: %v", err)
+	}
+
+	corsSection := cfg.Section("cors")
+	allowOrigins := strings.Split(corsSection.Key("allow_origins").String(), ",")
+	for i := range allowOrigins {
+		allowOrigins[i] = strings.TrimSpace(allowOrigins[i])
+	}
+
+	allowMethods := strings.Split(corsSection.Key("allow_methods").String(), ",")
+	for i := range allowMethods {
+		allowMethods[i] = strings.TrimSpace(allowMethods[i])
+	}
+
+	allowHeaders := strings.Split(corsSection.Key("allow_headers").String(), ",")
+	for i := range allowHeaders {
+		allowHeaders[i] = strings.TrimSpace(allowHeaders[i])
+	}
+
+	allowCredentials, err := corsSection.Key("allow_credentials").Bool()
+	if err != nil {
+		log.Fatalf("Failed to parse allow_credentials: %v", err)
+	}
+
+	return cors.Config{
+		AllowOrigins:     allowOrigins,
+		AllowMethods:     allowMethods,
+		AllowHeaders:     allowHeaders,
+		AllowCredentials: allowCredentials,
+	}
+}
--- cmd/server.go
+++ cmd/server.go
@@ -1,22 +1,29 @@
 package server
 
 import (
-	apis "Cosomos-Odyssey/internal/API"
+	"github.com/T4NKER/Cosmos-Odyssey/internal/API"
+	"github.com/T4NKER/Cosmos-Odyssey/internal/API/apis"
+	"github.com/T4NKER/Cosmos-Odyssey/internal/services"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
 )
 
 func Start() {
 	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-    AllowOrigins:     []string{"http://localhost:5173", "http://127.0.0.1:5173"},
-    AllowMethods:     []string{"PUT", "GET", "POST", "DELETE", "OPTIONS"},
-	AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
-    AllowCredentials: true,
-  }))
-	apis.Api(router)
-	router.Run()
+	router.Use(cors.New(loadCorsConfig()))
 
+	routeService := services.NewRouteService()
+	reservationService := services.NewReservationService()
+
+	routeAPI := apis.NewRouteAPI(routeService)
+	reservationAPI := apis.NewReservationAPI(reservationService)
+
+	masterAPI := API.NewMasterAPI(routeAPI, reservationAPI)
+	masterAPI.RegisterRoutes(router)
+
+	router.Run()
 }
+
 
