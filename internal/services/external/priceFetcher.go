4b5ac21d288b568cff4cec87f7d03615077db72a
mmuuripe <markus.muuripeal@gmail.com>
2024-06-27 12:12:27 +0300

    fixed pricefetcher with timer

--- internal/services/external/priceFetcher.go
+++ internal/services/external/priceFetcher.go
@@ -14,44 +14,56 @@ import (
 
 var AllConnections = make(map[string][]string)
 var pricelistService *PricelistService
-var once sync.Once
 
 type PricelistService struct {
 	Pricelist models.Pricelist
 	Mutex     sync.Mutex
-	DB        *sql.DB 
+	DB        *sql.DB
 }
 
-func NewExternalPricelistService() *PricelistService {
-	once.Do(func() {
-		pricelistService = &PricelistService{}
-		pricelistService.refreshPricelist()
-		go pricelistService.pricelistUpdater()
-	})
+func NewExternalPricelistService(DB *sql.DB) *PricelistService {
+	pricelistService = &PricelistService{
+		Pricelist: models.Pricelist{},
+		DB:        DB,
+		Mutex:     sync.Mutex{},
+	}
+	pricelistService.refreshPricelist()
+	go pricelistService.pricelistUpdater()
+
 	return pricelistService
 }
 
 func (p *PricelistService) pricelistUpdater() {
 	for {
-		time.Sleep(time.Minute)
-
 		p.Mutex.Lock()
-		if p.Pricelist.ValidUntil.Before(time.Now()) {
-			p.refreshPricelist()
-			log.Println("Pricelist refreshed.")
-		}
+		duration := time.Until(p.Pricelist.ValidUntil)
+		log.Println("Setting new duration to ", duration)
 		p.Mutex.Unlock()
+
+		timer := time.NewTimer(duration)
+		log.Println("Timer set for duration: ", duration)
+		<-timer.C
+		log.Println("Timer reached end, getting API response")
+
+		log.Println("Refreshing pricelist...")
+		p.refreshPricelist()
+		log.Println("Pricelist refreshed.")
 	}
 }
 
 func (p *PricelistService) refreshPricelist() {
+	log.Println("Fetching data...")
 	pricelistData := fetchAndStoreData()
+	log.Println("Data fetched, updating pricelist...")
 	p.Mutex.Lock()
 	defer p.Mutex.Unlock()
+	log.Println("Making new pricelist...")
 	p.Pricelist = pricelistData
+	log.Println("New pricelist made...")
 }
 
 func fetchAndStoreData() models.Pricelist {
+	log.Println("Starting fetchAndStoreData")
 	response, err := http.Get("https://cosmos-odyssey.azurewebsites.net/api/v1.0/TravelPrices")
 	if err != nil {
 		log.Fatalf("Failed to fetch data: %v", err)
@@ -59,16 +71,16 @@ func fetchAndStoreData() models.Pricelist {
 	defer response.Body.Close()
 
 	var pricelistData models.Pricelist
+	log.Println("Decoding response...")
 	if err := json.NewDecoder(response.Body).Decode(&pricelistData); err != nil {
 		log.Fatalf("Failed to decode data: %v", err)
 	}
 
-	// Store pricelist data in the database
+	log.Println("Storing pricelist data in the database...")
 	storePricelist(database.Db, pricelistData)
 
-	// Update in-memory connections
 	updateConnections(pricelistData)
-
+	log.Println("Pricelist data fetched and stored successfully")
 	return pricelistData
 }
 
@@ -115,6 +127,8 @@ func storePricelist(db *sql.DB, pricelist models.Pricelist) {
 }
 
 func updateConnections(pricelist models.Pricelist) {
+	AllConnections = make(map[string][]string)
+
 	for _, route := range pricelist.Legs {
 		fromPlanet := route.RouteInfo.From.Name
 		toPlanet := route.RouteInfo.To.Name
